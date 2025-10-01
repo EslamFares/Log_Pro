922e72c0c13e8a5316479fdfdeafbdc266ad5dac
EslamFaresGT <eslamfares@gulfterminal.net>
2025-10-01 11:21:16 +0300

    make every line print if start "/n" v(0.0.9)

--- lib/log_pro.dart
+++ lib/log_pro.dart
@@ -344,8 +344,13 @@ class LogPro {
 //!=========================================================================
   void _usePrintHandel(String msg, {required bool usePrint}) {
     if (usePrint) {
-      // ignore: avoid_print
-      print(msg);
+      // `print` can truncate long strings, so we split the message into lines
+      // or chunks to ensure everything is printed.
+      final lines = msg.split('\n');
+      for (final line in lines) {
+        // ignore: avoid_print
+        print(line);
+      }
     } else {
       log(msg);
     }
